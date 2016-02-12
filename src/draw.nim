411ad4eadadb0daaf1018edcbcbe5fdb3ec25f68
Piotr Klibert <piotr.klibert@10clouds.com>
2016-02-12 19:58:59 +0100

    Simplified drawing

--- src/draw.nim
+++ src/draw.nim
@@ -1,5 +1,6 @@
 import cairo
 import xlib, x
+import strutils
 
 import locks
 
@@ -16,32 +17,16 @@ proc set_font_size_px(ctx:PContext, screen:PScreen, px:int) =
 
 proc draw_something*(surface:PSurface, input:string, screen:PScreen) =
   let
-    (w,h) = (screen.width.toFloat(), screen.height.toFloat())
+    (w, h) = (screen.width.toFloat(), screen.height.toFloat())
     ctx = create(surface)
-
   defer:
-      destroy(ctx)
+    ctx.destroy()
 
   ctx.scale(w, h)
 
-  ctx.set_font_size(0.5)
-  ctx.set_source_rgb(0.0, 0.0, 0.0)
+  ctx.set_font_size_px(screen, 28)
   ctx.select_font_face("Georgia", FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL)
-  ctx.move_to(0, 0.5)
+  ctx.move_to(0.4, 0.5)
   ctx.set_source_rgb(0, 0, 0)
-  show_text(ctx, input)
 
-  ctx.set_font_size_px(screen, 45)
-  ctx.set_source_rgb(0.0, 1.0, 0.0)
-  ctx.select_font_face("Georgia", FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL)
-  ctx.move_to(0, 0.5)
-  ctx.set_source_rgb(0, 0, 0)
-  ctx.rotate(0.34)
-  show_text(ctx, "Yaaay...! " & input)
-  ctx.rotate(-0.34)
-
-  ctx.set_source_rgba(1, 1, 1, 1)
-  ctx.select_font_face("Georgia", FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL)
-  ctx.set_font_size_px(screen, 52)
-  ctx.move_to_px(screen, 0, 300)
-  ctx.show_text("OMG: " & input)
+  show_text(ctx, "> " & "*".repeat(len(input)))
